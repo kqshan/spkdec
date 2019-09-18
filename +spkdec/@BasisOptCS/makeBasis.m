5f5109f63d22a6db3853454393ae21f2449d2299
Kevin Shan <kshan@caltech.edu>
2019-09-18 15:37:13 -0700

    Forward add'l args from makeBasis() to optimize()

--- +spkdec/@BasisOptCS/makeBasis.m
+++ +spkdec/@BasisOptCS/makeBasis.m
@@ -10,6 +10,7 @@ function [basis_obj, spk] = makeBasis(self, spikes, D, varargin)
 %   D           Desired number of spike basis waveforms (must be divisible by C)
 % Optional parameters (key/value pairs) [default]:
 %   t0          Sample index (1..L) for t=0                         [ 1 ]
+%   ...         Add'l params are forwarded to self.optimize()
 %
 % This initializes the basis waveforms using a singular value decomposition
 % (SVD) on the given spike waveforms, then solves the optimization problem
--- +spkdec/@BasisOptimizer/makeBasis.m
+++ +spkdec/@BasisOptimizer/makeBasis.m
@@ -10,6 +10,7 @@ function [basis_obj, spk] = makeBasis(self, spikes, D, varargin)
 %   D           Desired number of spike basis waveforms
 % Optional parameters (key/value pairs) [default]:
 %   t0          Sample index (1..L) for t=0                         [ 1 ]
+%   ...         Add'l params are forwarded to self.optimize()
 %
 % This initializes the basis waveforms using a singular value decomposition
 % (SVD) on the given spike waveforms, then solves the optimization problem
@@ -17,12 +18,14 @@ function [basis_obj, spk] = makeBasis(self, spikes, D, varargin)
 
 % Parse optional inputs
 ip = inputParser();
+ip.KeepUnmatched = true; ip.PartialMatching = false;
 ip.addParameter('t0', 1, @isscalar);
 ip.parse( varargin{:} );
 prm = ip.Results;
+addl_args = ip.Unmatched;
 
 % Fit a spike basis to the given data
-[basis, spk] = self.optimize(spikes, 'lambda',0, 'D',D);
+[basis, spk] = self.optimize(spikes, 'lambda',0, 'D',D, addl_args);
 
 % Flip the signs so that the average spike feature is positive
 sgn = sign(mean(spk.X,2));
